25bddadde84c3eb5305ba647e18d982e552b4c57
Olivier Conard <conard@Oliviers-MBP.hitronhub.home>
2018-07-08 08:39:49 -0600

    Initial Commint

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  log = `${now}: ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('Server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log')
+    }
+  })
+  next();
+})
+
+// app.use((reg, res, next) => {
+//   res.render('maintenance.hbs', {
+//     pageTitle: "Maintenance"
+//   })
+// })
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', ()=> {
+  return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt', (text) =>{
+  return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+//res.send('<h1>hello express</h1>');
+res.render('home.hbs', {
+  pageTitle: 'Home Page',
+  WelcomeMessage: 'Welcome to my webstie',
+  currentYear: new Date().getFullYear()
+
+})
+})
+
+app.get('/bad', (req, res) => {
+  res.send( {
+    errorMessage: 'BAD_REQUEST'
+
+
+  })
+})
+
+app.get('/about', (req, res) => {
+res.render('about.hbs', {
+  pageTitle: 'About Page',
+  currentYear: new Date().getFullYear()
+});
+})
+app.listen(3000, () => {
+  console.log("Server started on port 3000")
+});
